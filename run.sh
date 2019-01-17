cc48b24c7df3e523cb65efc614874f0a37d9f6bf
yh姚宏 <hongyao@ctrip.com>
2019-01-17 20:13:15 +0800

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ~/grpc-proxy-demo
 sudo ./env.sh
-docker run -p 80:80 --name grpc-proxy -v $PWD/www:/www -v $PWD/nginx/nginx.conf:/etc/nginx/nginx.conf  -d hub.cloud.ctripcorp.com/cbaas/nginx:latest
+sudo mkdir -p /var/log/nginx
+docker run -p 80:80 --name grpc-proxy -v $PWD/www:/www -v $PWD/nginx/nginx.conf:/etc/nginx/nginx.conf -v /var/log/nginx:/var/log/nginx  -d hub.cloud.ctripcorp.com/cbaas/nginx:latest
